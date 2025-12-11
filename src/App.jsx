d36fdbf604c8db08abf96d0c998f38b92a69c922
PratikVadaviya <pratikpatel9681@gmail.com>
2025-12-11 18:32:53 +0530

    tested HeroSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,16 @@
-// import HeroSection from './components/sections/HeroSection'
+import HeroSection from './components/sections/HeroSection.jsx'
 
 
-// function App() {
+function App() {
 
-//   return (
-//     <>
-//      <main>
-//         <HeroSection />
-        
-//       </main>
-//     </>
-//   )
-// }
-
-//export default App
-
-// src/App.jsx
-// export default function App() {
-//   return (
-//     <h1 className="text- text-5xl font-bold">Test Red Color</h1>
-//   );
-// }
-
-
-export default function App() {
   return (
-    <h1 className="text-3xl bg-red-500 font-bold underline">
-      Hello world!
-    </h1>
+    <>
+     <main>
+        <HeroSection />
+        
+      </main>
+    </>
   )
 }
+
+export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/*/.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      colors: {
-        primary: "#FF2F92",
-        accent: "#1B4FFF",
-        secondary: "#5B3FFF",
-        textMain: "#1A1A1A",
-        bgLight: "#FFF7FB",
-      },
+       colors: {
+      primary: '#FF2F92',
+      accent: '#1B4FFF',
+      textMain: '#1A1A1A',
+      bgLight: '#FFF7FB',
+    },
     },
   },
   plugins: [],
 }
+
